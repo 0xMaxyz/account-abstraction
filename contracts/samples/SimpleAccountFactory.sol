f551d356832c19b69f67262ab40bfa5508788371
Saeed <hamidradpj@gmail.com>
2024-03-28 00:02:35 +0300

    new simpleAccountFactory with unique name as the salt

--- contracts/samples/SimpleAccountFactory.sol
+++ contracts/samples/SimpleAccountFactory.sol
@@ -15,6 +15,9 @@ import "./SimpleAccount.sol";
 contract SimpleAccountFactory {
     SimpleAccount public immutable accountImplementation;
 
+    mapping(uint256 => address) public AddressOf;
+    error NameExists(uint256 name);
+
     constructor(IEntryPoint _entryPoint) {
         accountImplementation = new SimpleAccount(_entryPoint);
     }
@@ -25,28 +28,67 @@ contract SimpleAccountFactory {
      * Note that during UserOperation execution, this method is called only if the account is not deployed.
      * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation
      */
-    function createAccount(address owner,uint256 salt) public returns (SimpleAccount ret) {
-        address addr = getAddress(owner, salt);
-        uint256 codeSize = addr.code.length;
-        if (codeSize > 0) {
-            return SimpleAccount(payable(addr));
+    function createAccount(
+        address owner,
+        uint256 name
+    ) public returns (SimpleAccount ret) {
+        address addr = _getAddress(owner, name);
+        address fromMapping = AddressOf[name];
+
+        if (fromMapping != address(0) && addr == fromMapping) {
+            return SimpleAccount(payable(fromMapping));
         }
-        ret = SimpleAccount(payable(new ERC1967Proxy{salt : bytes32(salt)}(
-                address(accountImplementation),
-                abi.encodeCall(SimpleAccount.initialize, (owner))
-            )));
+        // The SimpleAccount for "name" is deployed for another owner, duplicate names are not allowed
+        else if (fromMapping != address(0) && addr != fromMapping) {
+            revert NameExists(name);
+        }
+        // The input "name" is not deployed before
+        else {
+            uint256 codeSize = addr.code.length;
+            if (codeSize > 0) {
+                return SimpleAccount(payable(addr));
+            }
+            ret = SimpleAccount(
+                payable(
+                    new ERC1967Proxy{salt: bytes32(name)}(
+                        address(accountImplementation),
+                        abi.encodeCall(SimpleAccount.initialize, (owner))
+                    )
+                )
+            );
+            AddressOf[name] = addr;
+        }
     }
 
-    /**
-     * calculate the counterfactual address of this account as it would be returned by createAccount()
-     */
-    function getAddress(address owner,uint256 salt) public view returns (address) {
-        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
-                type(ERC1967Proxy).creationCode,
-                abi.encode(
-                    address(accountImplementation),
-                    abi.encodeCall(SimpleAccount.initialize, (owner))
+    function _getAddress(
+        address owner,
+        uint256 name
+    ) private view returns (address) {
+        return
+            Create2.computeAddress(
+                bytes32(name),
+                keccak256(
+                    abi.encodePacked(
+                        type(ERC1967Proxy).creationCode,
+                        abi.encode(
+                            address(accountImplementation),
+                            abi.encodeCall(SimpleAccount.initialize, (owner))
+                        )
+                    )
                 )
-            )));
+            );
+    }
+
+    function getAddress(
+        address owner,
+        uint256 name
+    ) external view returns (address) {
+        address addr = _getAddress(owner, name);
+        address fromMapping = AddressOf[name];
+        if (fromMapping != address(0) && addr != fromMapping) {
+            revert NameExists(name);
+        }
+
+        return _getAddress(owner, name);
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,24 +8,35 @@ import 'solidity-coverage'
 
 import * as fs from 'fs'
 
-const SALT = '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3'
+const SALT =
+  '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3'
 process.env.SALT = process.env.SALT ?? SALT
 
-task('deploy', 'Deploy contracts')
-  .addFlag('simpleAccountFactory', 'deploy sample factory (by default, enabled only on localhost)')
+task('deploy', 'Deploy contracts').addFlag(
+  'simpleAccountFactory',
+  'deploy sample factory (by default, enabled only on localhost)'
+)
 
 const mnemonicFileName = process.env.MNEMONIC_FILE!
 let mnemonic = 'test '.repeat(11) + 'junk'
-if (fs.existsSync(mnemonicFileName)) { mnemonic = fs.readFileSync(mnemonicFileName, 'ascii') }
+if (fs.existsSync(mnemonicFileName)) {
+  mnemonic = fs.readFileSync(mnemonicFileName, 'ascii')
+}
 
-function getNetwork1 (url: string): { url: string, accounts: { mnemonic: string } } {
+function getNetwork1(url: string): {
+  url: string
+  accounts: { mnemonic: string }
+} {
   return {
     url,
     accounts: { mnemonic }
   }
 }
 
-function getNetwork (name: string): { url: string, accounts: { mnemonic: string } } {
+function getNetwork(name: string): {
+  url: string
+  accounts: { mnemonic: string }
+} {
   return getNetwork1(`https://${name}.infura.io/v3/${process.env.INFURA_ID}`)
   // return getNetwork1(`wss://${name}.infura.io/ws/v3/${process.env.INFURA_ID}`)
 }
@@ -43,12 +54,14 @@ const optimizedComilerSettings = {
 
 const config: HardhatUserConfig = {
   solidity: {
-    compilers: [{
-      version: '0.8.23',
-      settings: {
-        optimizer: { enabled: true, runs: 1000000 }
+    compilers: [
+      {
+        version: '0.8.23',
+        settings: {
+          optimizer: { enabled: true, runs: 1000000 }
+        }
       }
-    }],
+    ],
     overrides: {
       'contracts/core/EntryPoint.sol': optimizedComilerSettings,
       'contracts/samples/SimpleAccount.sol': optimizedComilerSettings
@@ -61,7 +74,11 @@ const config: HardhatUserConfig = {
     goerli: getNetwork('goerli'),
     sepolia: getNetwork('sepolia'),
     proxy: getNetwork1('http://localhost:8545'),
-    botanix: getNetwork1('https://node.botanixlabs.dev')
+    botanix: {
+      url: 'https://node.botanixlabs.dev',
+      chainId: 3636,
+      accounts: { mnemonic }
+    }
   },
   mocha: {
     timeout: 10000
@@ -70,7 +87,6 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
   }
-
 }
 
 // coverage chokes on the "compilers" settings
